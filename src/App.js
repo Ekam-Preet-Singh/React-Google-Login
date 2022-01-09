54b5d5c668ad0ced77aca1d06cf445cb76199f7a
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-01-09 20:05:50 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GoogleLogin from "react-google-login";
+import { Fragment, useState } from "react";
+import Container from "@material-ui/core/Container";
+import loginImg from "./login.png";
+import Topography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { FcGoogle } from "react-icons/fc";
 
 function App() {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+
+  const handleFailure = (result) => {
+    alert(result);
+  };
+
+  const handleLogin = async (googleData) => {
+    const res = await fetch("/api/google-login", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(googleData);
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem("loginData", JSON.stringify(data));
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="div" maxWidth="xs" className="root">
+      <div className="paper">
+        <div>
+          {loginData ? (
+            <Fragment>
+              <Topography variant="h6" style={{ paddingTop: "15px" }}>
+                <strong>You logged in as</strong> {loginData.email}
+              </Topography>
+              <Button
+                variant="outlined"
+                color="primary"
+                className="submit"
+                style={{ marginTop: "15px", marginLeft: "150px" }}
+                startIcon={<FcGoogle />}
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Topography variant="h4" style={{ paddingTop: "15px" }}>
+                React Google Login App
+              </Topography>
+              <img src={loginImg} className="mainImg" alt="signup img" />
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Log in with Google"
+                onSuccess={handleLogin}
+                onFailure={handleFailure}
+                cookiePolicy={"single_host_origin"}
+                className="submit"
+              ></GoogleLogin>
+            </Fragment>
+          )}
+        </div>
+      </div>
+    </Container>
   );
 }
 
